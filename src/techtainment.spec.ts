682f4e8732e7efec69787ddd5b5957f632ed0f1c
Nathan Arthur <nathan@nathanarthur.com>
2022-10-18 20:28:29 -0400

    limit techtainment to last week

--- src/techtainment.spec.ts
+++ src/techtainment.spec.ts
@@ -2,22 +2,37 @@ import { describe, it, expect, vi, beforeEach } from "vitest";
 import getGoal from "./lib/bm/getGoal";
 import techtainment from "./techtainment";
 import createDatapoint from "./lib/bm/createDatapoint";
-import { Goal } from "./lib/bm/types";
+import { Datapoint, Goal } from "./lib/bm/types";
 
 vi.mock("./lib/bm/getGoal");
 vi.mock("./lib/bm/createDatapoint");
 
+function loadDatapoints(
+  datapoints: Partial<Datapoint>[] = [
+    {
+      daystamp: "20210101",
+      value: 1,
+    },
+  ]
+) {
+  vi.mocked(getGoal).mockResolvedValue({ datapoints } as Goal);
+}
+
+function expectValue(value: number) {
+  expect(createDatapoint).toBeCalledWith(
+    "narthur",
+    "techtainment",
+    expect.objectContaining({
+      value,
+    })
+  );
+}
+
 describe("techtainment", () => {
   beforeEach(() => {
     vi.setSystemTime(new Date("2021-01-01T00:00:00.000Z"));
-    vi.mocked(getGoal).mockResolvedValue({
-      datapoints: [
-        {
-          daystamp: "20210101",
-          value: 1,
-        },
-      ],
-    } as Goal);
+    loadDatapoints();
+    vi.mocked(createDatapoint).mockClear();
   });
 
   it("gets exercise Beeminder goal", async () => {
@@ -29,13 +44,7 @@ describe("techtainment", () => {
   it("creates techtainment datapoint", async () => {
     await techtainment();
 
-    expect(createDatapoint).toBeCalledWith(
-      "narthur",
-      "techtainment",
-      expect.objectContaining({
-        value: -2,
-      })
-    );
+    expectValue(-2);
   });
 
   it("sets request id", async () => {
@@ -63,48 +72,45 @@ describe("techtainment", () => {
   });
 
   it("calculates value accurately", async () => {
-    vi.mocked(getGoal).mockResolvedValue({
-      datapoints: [
-        {
-          daystamp: "20210101",
-          value: 3,
-        },
-      ],
-    } as Goal);
+    loadDatapoints([
+      {
+        daystamp: "20210101",
+        value: 3,
+      },
+    ]);
 
     await techtainment();
 
-    expect(createDatapoint).toBeCalledWith(
-      "narthur",
-      "techtainment",
-      expect.objectContaining({
-        value: -6,
-      })
-    );
+    expectValue(-6);
   });
 
   it("sums day values", async () => {
-    vi.mocked(getGoal).mockResolvedValue({
-      datapoints: [
-        {
-          daystamp: "20210101",
-          value: 1,
-        },
-        {
-          daystamp: "20210101",
-          value: 2,
-        },
-      ],
-    } as Goal);
+    loadDatapoints([
+      {
+        daystamp: "20210101",
+        value: 1,
+      },
+      {
+        daystamp: "20210101",
+        value: 2,
+      },
+    ]);
 
     await techtainment();
 
-    expect(createDatapoint).toBeCalledWith(
-      "narthur",
-      "techtainment",
-      expect.objectContaining({
-        value: -6,
-      })
-    );
+    expectValue(-6);
+  });
+
+  it("only creates datapoints for last 7 days", async () => {
+    loadDatapoints([
+      {
+        daystamp: "20200101",
+        value: 1,
+      },
+    ]);
+
+    await techtainment();
+
+    expect(createDatapoint).not.toBeCalled();
   });
 });
--- src/techtainment.ts
+++ src/techtainment.ts
@@ -1,10 +1,37 @@
 import createDatapoint from "./lib/bm/createDatapoint";
 import getGoal from "./lib/bm/getGoal";
 
+// https://stackoverflow.com/a/10639010/937377
+function parse(str: string) {
+  // validate year as 4 digits, month as 01-12, and day as 01-31
+  const m = str.match(/^(\d{4})(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])$/);
+
+  if (!m) {
+    throw new Error(`Invalid date string: ${str}`);
+  }
+
+  const d = new Date(+m[1], +m[2] - 1, +m[3]);
+
+  // check if month stayed the same (ie that day number is valid)
+  if (d.getMonth() === +m[2] - 1) return d;
+}
+
 export default async function techtainment() {
   const exercise = await getGoal("narthur", "exercise");
   const daysums = exercise.datapoints.reduce((acc, datapoint) => {
+    // YYYYMMDD
     const daystamp = datapoint.daystamp;
+
+    // Check if the daystamp is within the last 7 days.
+    const today = new Date();
+    const day = parse(daystamp);
+    const diff = Math.floor(
+      (today.getTime() - day.getTime()) / (1000 * 60 * 60 * 24)
+    );
+    if (diff > 7) {
+      return acc;
+    }
+
     acc[daystamp] = (acc[daystamp] || 0) + datapoint.value;
     return acc;
   }, {} as Record<string, number>);
