5d41ba64288cf0363a2150b4b200a7fb8ffa478f
Nathan Arthur <nathan@nathanarthur.com>
2023-02-20 16:55:26 -0500

    use https trigger for gross

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import * as functions from "firebase-functions";
 import gross_ from "./jobs/gross";
 
-const gross = functions.pubsub
-  .schedule("every 10 minutes")
-  .onRun(() => gross_());
+const gross = functions.https.onRequest(gross_);
 
 export { gross };
