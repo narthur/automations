e7fc7ccd9dd05a42c9bc9c92f4a48809b4e61e04
Nathan Arthur <nathan@nathanarthur.com>
2023-02-21 09:55:14 -0500

    handle toggl validation PING events

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,12 @@ const gross = functions
     secrets: [bmAuths.name, togglApiToken.name],
   })
   .https.onRequest(async (req, res) => {
+    if (req.params.validation_code) {
+      // https://developers.track.toggl.com/docs/webhooks_start/url_endpoint_validation/index.html
+      res.send({
+        validation_code: req.params.validation_code,
+      });
+    }
     await gross_();
     res.send("job completed: gross");
   });
