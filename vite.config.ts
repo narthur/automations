e86af734a4e285b50894d0e872dae647afec63d3
Nathan Arthur <nathan@nathanarthur.com>
2023-03-01 09:00:25 -0500

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,10 @@
 import { defineConfig } from "vitest/config";
-import { ViteAliases } from "vite-aliases";
 
 export default defineConfig({
-  plugins: [
-    ViteAliases({
-      useConfig: true,
-      useTypescript: true,
-    }),
-  ],
   test: {
     root: __dirname,
     setupFiles: ["./vitest.setup.ts"],
     clearMocks: true,
     include: ["./src/**/*.spec.ts"],
-  },
-  build: {
-    outDir: "lib",
-    lib: {
-      entry: "src/index.ts",
-      formats: ["cjs"],
-    },
   },
 });
