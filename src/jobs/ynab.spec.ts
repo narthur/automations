48d650b8574f1479986d279060f1a39953688605
Nathan Arthur <nathan@nathanarthur.com>
2023-06-18 11:51:20 -0400

    wip

--- src/jobs/ynab.spec.ts
+++ src/jobs/ynab.spec.ts
@@ -7,3 +7,9 @@ describe("ynab", () => {
     expect(true).toBe(true);
   });
 });
+
+// it checks when budget was last reconciled... ?
+// tracks amount of money unassigned
+// https://api.youneedabudget.com/#quick-start
+// https://api.youneedabudget.com/v1#/User/getUser
+// https://app.youneedabudget.com/settings/developer
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import { ViteAliases } from "vite-aliases";
 
 export default defineConfig({
+  plugins: [ViteAliases()],
   test: {
     root: __dirname,
     setupFiles: ["./vitest.setup.ts"],
