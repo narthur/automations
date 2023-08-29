076722596fe879f5aac0eef704d979c79c66c80e
Nathan Arthur <nathan@nathanarthur.com>
2023-08-29 10:43:08 -0400

    jsdoc tweak

--- src/services/taskratchet.ts
+++ src/services/taskratchet.ts
@@ -38,9 +38,9 @@ export function getPendingTasks() {
 }
 
 /**
- * Create a TaskRatchet task
+ * Create a TaskRatchet task.
  * Docs: https://taskratchet.com/help/api.html#schema
- * 
+ *
  * @param task - Task title
  * @param due - Due date and time in string of format 3/25/2020, 11:59 PM
  * @param cents - Stakes in cents
@@ -50,6 +50,6 @@ export function createTask(task: string, due: string, cents: number) {
   return getClient().post<Task>("/me/tasks", {
     task,
     due,
-    cents
+    cents,
   });
 }
