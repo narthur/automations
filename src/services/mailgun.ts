0dd9207fe45ae26aa7e6fd2cc76468af0dd16edb
Nathan Arthur <nathan@nathanarthur.com>
2023-09-01 14:08:13 -0400

    switch to mailgun.js

--- src/services/mailgun.ts
+++ src/services/mailgun.ts
@@ -1,14 +1,15 @@
-// import axios, { AxiosError } from "axios";
 import { mailgunApiKey, mailgunDomain } from "../secrets";
-
 import FormData from "form-data";
 import Mailgun from "mailgun.js";
+import memoize from "../effects/memoize";
 
-const mailgun = new Mailgun(FormData);
-const mg = mailgun.client({
-  username: "api",
-  key: mailgunApiKey.value(),
-});
+const getMailgun = memoize(() => {
+  const mailgun = new Mailgun(FormData);
+  return mailgun.client({
+    username: "api",
+    key: mailgunApiKey.value(),
+  });
+}, "mailgun");
 
 export async function sendEmail({
   recipients = ["nathan@nathanarthur.com"],
@@ -22,54 +23,10 @@ export async function sendEmail({
   from?: string;
 }): Promise<unknown> {
   console.info("Sending email", { recipients, subject });
-  return mg.messages.create(mailgunDomain.value(), {
+  return getMailgun().messages.create(mailgunDomain.value(), {
     from: `Nathan Arthur <${from}>`,
     to: recipients,
     subject,
     text: body,
   });
 }
-
-// export async function sendEmail({
-//   recipients = ["nathan@nathanarthur.com"],
-//   body,
-//   from = "nathan@nathanarthur.com",
-//   subject,
-// }: {
-//   recipients?: string[];
-//   body: string;
-//   subject: string;
-//   from?: string;
-// }): Promise<unknown> {
-//   console.info("Sending email", { recipients, subject });
-//   return axios
-//     .post(
-//       `https://api.mailgun.net/v3/${mailgunDomain.value()}/messages`,
-//       {
-//         from: `Nathan Arthur <${from}>`,
-//         to: recipients,
-//         subject,
-//         text: body,
-//       },
-//       {
-//         auth: {
-//           username: "api",
-//           password: mailgunApiKey.value(),
-//         },
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       }
-//     )
-//     .catch((e) => {
-//       console.error("Error sending email");
-//       if (e instanceof AxiosError) {
-//         console.error(e.toJSON());
-//       }
-//       if (e instanceof Error) {
-//         console.error(e.message);
-//         console.error(e.stack);
-//       }
-//       throw e;
-//     });
-// }
