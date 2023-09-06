f5546d45a4875285a43dcd161ace1907b42eec49
Nathan Arthur <nathan@nathanarthur.com>
2023-09-06 19:24:49 -0400

    console.info

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,4 @@ import "./cron.ts";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Listening at http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.info(`Listening at http://localhost:${PORT}`));
