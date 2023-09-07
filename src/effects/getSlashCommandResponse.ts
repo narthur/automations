3c59002efed959312d2e02357949198dc381bce8
Nathan Arthur <nathan@nathanarthur.com>
2023-09-07 16:30:30 -0400

    refactor slash command handling

--- src/effects/getSlashCommandResponse.ts
+++ src/effects/getSlashCommandResponse.ts
@@ -3,44 +3,51 @@ import getBeemergencies from "./getBeemergencies.js";
 import { clearHistory } from "./history.js";
 import os from "os";
 
+const commands: {
+  match: RegExp;
+  action: (message: string) => Promise<string[]> | string[];
+}[] = [];
+
+function s(
+  match: RegExp,
+  action: (message: string) => Promise<string[]> | string[]
+) {
+  commands.push({ match, action });
+}
+
+s(/^\/foo$/, () => ["bar"]);
+
+s(/^\/uptime$/, () => [
+  `process: ${process.uptime()}s`,
+  `system: ${os.uptime()}s`,
+]);
+
+s(/^\/time$/, () => [`The time is ${new Date().toLocaleTimeString()}`]);
+
+s(/^\/date$/, () => [`The date is ${new Date().toLocaleDateString()}`]);
+
+s(/^\/reset$/, () => {
+  clearHistory();
+  return ["Internal memory cleared"];
+});
+
+s(/^\/beemergencies$/, async () => [await getBeemergencies()]);
+
+s(/^\/taskratchet pending$/, async () => {
+  const tasks = await getPendingTasks();
+  return tasks.map((t) => `${t.task} due ${t.due} or pay $${t.cents / 100}`);
+});
+
+s(/^\/roll (\d+)$/, (message) => {
+  const [, sides] = message.match(/^\/roll (\d+)$/) || [];
+  const roll = Math.floor(Math.random() * Number(sides)) + 1;
+  return [`You rolled a ${roll}`];
+});
+
 export default async function getSlashCommandResponse(
   message: string
 ): Promise<string[] | false> {
-  if (message === "/foo") {
-    return ["bar"];
-  }
+  const m = commands.find((c) => c.match.test(message));
 
-  if (message === "/uptime") {
-    return [`process: ${process.uptime()}s`, `system: ${os.uptime()}s`];
-  }
-
-  if (message === "/time") {
-    return [`The time is ${new Date().toLocaleTimeString()}`];
-  }
-
-  if (message === "/date") {
-    return [`The date is ${new Date().toLocaleDateString()}`];
-  }
-
-  if (message === "/reset") {
-    clearHistory();
-    return ["Internal memory cleared"];
-  }
-
-  if (message === "/beemergencies") {
-    return [await getBeemergencies()];
-  }
-
-  if (message === "/taskratchet pending") {
-    const tasks = await getPendingTasks();
-    return tasks.map((t) => `${t.task} due ${t.due} or pay $${t.cents / 100}`);
-  }
-
-  if (message.match(/^\/roll (\d+)$/)) {
-    const [, sides] = message.match(/^\/roll (\d+)$/) || [];
-    const roll = Math.floor(Math.random() * Number(sides)) + 1;
-    return [`You rolled a ${roll}`];
-  }
-
-  return false;
+  return m ? m.action(message) : false;
 }
