7ddde233fd92d0a38b54134f850913faac4982a7
Nathan Arthur <nathan@nathanarthur.com>
2023-09-09 08:13:50 -0400

    throttle toggl api usage

--- src/services/toggl/index.ts
+++ src/services/toggl/index.ts
@@ -1,6 +1,15 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { togglApiToken } from "../../secrets.js";
 import { TimeEntry, TogglClient, TogglProject, TogglTask } from "./types.js";
+import PQueue from "p-queue";
+
+const client = axios.create({
+  baseURL: "https://api.track.toggl.com/api/v9",
+});
+
+const token = togglApiToken.value();
+const auth = Buffer.from(`${token}:api_token`).toString("base64");
+client.defaults.headers.common["Authorization"] = `Basic ${auth}`;
 
 export function getProjects(options: AxiosRequestConfig = {}) {
   return api<TogglProject[]>("me/projects", options);
@@ -39,26 +48,15 @@ export function getTasks(
   );
 }
 
+const queue = new PQueue({
+  intervalCap: 1,
+  interval: 1000,
+  throwOnTimeout: true,
+});
+
 async function api<T>(p: string, o: AxiosRequestConfig = {}): Promise<T> {
-  const u = `https://api.track.toggl.com/api/v9/${p}`;
-  const t = togglApiToken.value();
-  const a = Buffer.from(`${t}:api_token`).toString("base64");
-
-  const r = await axios(u, {
-    headers: {
-      Authorization: `Basic ${a}`,
-    },
-    ...o,
-  }).catch((err) => {
-    console.error("Error while calling Toggl API", {
-      endpoint: p,
-      url: u,
-      options: o,
-      token: t,
-      auth: a,
-    });
-    throw err;
-  });
-
-  return r.data as T;
+  const result = await queue.add(() => axios(p, o).then((r) => r.data as T));
+  // WORKAROUND: https://github.com/sindresorhus/p-queue/issues/175
+  if (!result) throw new Error("No Toggl API result");
+  return result;
 }
