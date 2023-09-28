f62f5c6d50031a85c9768c4aecf5714f14ce072e
Nathan Arthur <nathan@nathanarthur.com>
2023-09-28 17:36:19 -0400

    refactor dynalist api wrapper

--- src/services/dynalist.ts
+++ src/services/dynalist.ts
@@ -33,12 +33,9 @@ export const getFiles = makeRoute<
   }
 >("file/list");
 
-export const updateFile = makePostRoute<
-  {
-    changes: unknown;
-  },
-  unknown
->("file/edit");
+export const updateFile = makeRoute<{
+  changes: unknown;
+}>("file/edit");
 
 export const getDocument = makeRoute<
   {
@@ -65,43 +62,28 @@ export const getDocumentUpdates = makeRoute<
   }
 >("doc/check_for_updates");
 
-export const updateDocument = makePostRoute<
-  {
-    file_id: string;
-    changes: unknown;
-  },
-  unknown
->("doc/edit");
+export const updateDocument = makeRoute<{
+  file_id: string;
+  changes: unknown;
+}>("doc/edit");
 
-export const addToInbox = makePostRoute<
-  {
-    index: number;
-    content: string;
-    note?: string;
-    checked?: boolean;
-    checkbox?: boolean;
-    heading?: 0 | 1 | 2 | 3;
-    color?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
-  },
-  unknown
->("inbox/add");
+export const addToInbox = makeRoute<{
+  index: number;
+  content: string;
+  note?: string;
+  checked?: boolean;
+  checkbox?: boolean;
+  heading?: 0 | 1 | 2 | 3;
+  color?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+}>("inbox/add");
 
-export const uploadFile = makePostRoute<
-  {
-    filename: string;
-    content_type: string;
-    data: string; // base64 encoded
-  },
-  unknown
->("file/upload");
+export const uploadFile = makeRoute<{
+  filename: string;
+  content_type: string;
+  data: string; // base64 encoded
+}>("file/upload");
 
-function makePostRoute<T extends Record<string, unknown>, D>(
-  route: string
-): (params?: T) => Promise<D> {
-  return makeRoute<T, D>(route);
-}
-
-function makeRoute<T extends Record<string, unknown>, D>(
+function makeRoute<T extends Record<string, unknown>, D = unknown>(
   route: string
 ): (params?: T) => Promise<D> {
   return async (params): Promise<D> => {
