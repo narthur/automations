38caa020c42b8d4e4c6d0d50da7099b65d22b4e9
Nathan Arthur <nathan@nathanarthur.com>
2023-10-05 15:29:29 -0400

    refactor gross update

--- src/goals/gross.ts
+++ src/goals/gross.ts
@@ -1,29 +1,35 @@
 import { getMe } from "../services/toggl/index.js";
 import { createDatapoint } from "../services/beeminder.js";
-import { TogglMe, TogglTimeSummaryGroup } from "src/services/toggl/types.js";
+import {
+  TogglMe,
+  TogglTimeSummaryEntry,
+  TogglTimeSummaryGroup,
+} from "src/services/toggl/types.js";
 import getWeekDates from "src/effects/getWeekDates.js";
 import getTimeSummary from "src/services/toggl/getTimeSummary.js";
 
-function sumUser(user: TogglTimeSummaryGroup, me: TogglMe) {
-  const entries = user?.sub_groups ?? [];
-  const sum: number = entries.reduce((acc, entry) => {
-    const entrySum = entry.rates.reduce<number>((acc, rate) => {
-      const hours = rate.billable_seconds / 3600;
-      const dollarRate = rate.hourly_rate_in_cents / 100;
-      return acc + hours * dollarRate;
-    }, 0);
-    return acc + entrySum;
+function sumRates(rates: TogglTimeSummaryEntry["rates"]): number {
+  return rates.reduce<number>((acc, rate) => {
+    const hours = rate.billable_seconds / 3600;
+    const dollarRate = rate.hourly_rate_in_cents / 100;
+    return acc + hours * dollarRate;
   }, 0);
+}
 
+function sumEntries(entries: TogglTimeSummaryEntry[]): number {
+  return entries.reduce((acc, entry) => acc + sumRates(entry.rates), 0);
+}
+
+function sumUser(user: TogglTimeSummaryGroup, me: TogglMe): number {
+  const entries = user?.sub_groups ?? [];
   const multiplier = user.id === me.id ? 1 : 0.3;
-
-  return sum * multiplier;
+  return sumEntries(entries) * multiplier;
 }
 
 async function doUpdate(date: Date, me: TogglMe) {
   const daystamp = date.toISOString().split("T")[0];
 
-  const summary = await getTimeSummary({
+  const { groups } = await getTimeSummary({
     workspaceId: me.default_workspace_id,
     startDate: date,
     endDate: date,
@@ -31,12 +37,10 @@ async function doUpdate(date: Date, me: TogglMe) {
     grouping: "users",
   });
 
-  const sum: number = summary.groups.reduce((acc, user) => {
-    return acc + sumUser(user, me);
-  }, 0);
+  const value = groups.reduce((acc, user) => acc + sumUser(user, me), 0);
 
   await createDatapoint("narthur", "gross", {
-    value: sum,
+    value,
     daystamp,
     requestid: daystamp,
   });
