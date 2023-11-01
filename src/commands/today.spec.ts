0c96524175871b1684fc1cdb6627d2a7ca1122c0
Nathan Arthur <nathan@nathanarthur.com>
2023-11-01 10:04:16 -0400

    work on today command

--- src/commands/today.spec.ts
+++ src/commands/today.spec.ts
@@ -1,8 +1,15 @@
+import { getDueTasks } from "src/services/taskratchet.js";
 import runCommand from "../lib/runCommand.js";
 import { expect, it, describe } from "vitest";
 
 describe("today", () => {
   it("returns response", async () => {
     expect(await runCommand("/today")).toEqual([expect.any(String)]);
+  });
+
+  it("gets TaskRatchet tasks", async () => {
+    await runCommand("/today");
+
+    expect(getDueTasks).toBeCalled();
   });
 });
--- src/commands/today.ts
+++ src/commands/today.ts
@@ -1,6 +1,7 @@
+import { getDueTasks } from "src/services/taskratchet.js";
 import cmd from "../lib/cmd.js";
 
-export default cmd(
-  "today",
-  () => `Today is ${new Date().toLocaleDateString()}`
-);
+export default cmd("today", async () => {
+  await getDueTasks();
+  return "hello world";
+});
