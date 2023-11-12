6daedb59fada718e58dd61b3c16f793e27678061
Nathan Arthur <nathan@nathanarthur.com>
2023-11-12 09:52:02 -0500

    refactor alarm factory

--- src/alarms/index.ts
+++ src/alarms/index.ts
@@ -20,35 +20,25 @@ export function createAlarmTrigger(options: Options) {
     const items = await options.getItems();
     const sorted = items.sort((a: Item, b: Item) => a.timestamp - b.timestamp);
     const next = sorted[0];
-    const nextDate = new Date(next.timestamp * 1000).toLocaleDateString(
-      "en-US",
-      {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-      }
-    );
-    const nowDate = new Date(Date.now()).toLocaleDateString("en-US", {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
-    });
-    const isDueToday = nextDate === nowDate;
-    const isDueNow = isNotificationDue({
-      due: next.timestamp,
-      now: Date.now() / 1000,
-      window: 60,
-    });
 
     if (!next) {
       console.info(`${options.id}: no next item`);
       return;
     }
 
-    if (!isDueToday) {
-      console.info(`${options.id}: not due today: ${nextDate} !== ${nowDate}`);
+    const nextDate = new Date(next.timestamp * 1000).toLocaleDateString();
+    const today = new Date(Date.now()).toLocaleDateString();
+
+    if (nextDate !== today) {
+      console.info(`${options.id}: not due today: ${nextDate} !== ${today}`);
       return;
     }
+
+    const isDueNow = isNotificationDue({
+      due: next.timestamp,
+      now: Date.now() / 1000,
+      window: 60,
+    });
 
     if (!isDueNow) {
       console.info(`${options.id}: not due now: ${next.timestamp}`);
