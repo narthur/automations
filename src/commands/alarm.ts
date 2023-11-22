6de7ca6568caa81600ed774f0094e187fa0ea51d
Nathan Arthur <nathan@nathanarthur.com>
2023-11-22 08:30:24 -0500

    allow for delaying alarm

--- src/commands/alarm.ts
+++ src/commands/alarm.ts
@@ -1,3 +1,9 @@
 import cmd from "src/lib/cmd";
 
-export default cmd("alarm", () => "🚨 Here is your test alarm!");
+export default cmd("alarm", async (message) => {
+  const [, seconds = 0] = message.match(/^\/alarm (\d+)$/) || [];
+
+  await new Promise((resolve) => setTimeout(resolve, Number(seconds) * 1000));
+
+  return "🚨 Here is your test alarm!";
+});
