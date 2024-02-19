e5f2fb96559a1b58ec3161528d3aff6ed30b675d
Nathan Arthur <nathan@nathanarthur.com>
2024-02-19 09:28:03 -0500

    delete billable goal, toggl hook

--- src/cron.ts
+++ src/cron.ts
@@ -2,7 +2,6 @@ import { Cron } from "croner";
 
 import bm from "./alarms/bm.js";
 import tr from "./alarms/tr.js";
-import * as billable from "./goals/billable.js";
 import * as dynadone from "./goals/dynadone.js";
 import * as dynanew from "./goals/dynanew.js";
 import * as gross from "./goals/gross.js";
@@ -12,7 +11,6 @@ const HALF_HOUR = "0 */30 * * * *" as const;
 const TEN_MINUTES = "0 */10 * * * *" as const;
 const MINUTE = "0 * * * * *" as const;
 
-new Cron(HALF_HOUR, billable.update);
 new Cron(HALF_HOUR, dynadone.update);
 new Cron(HALF_HOUR, dynanew.update);
 new Cron(MINUTE, bm.send);
