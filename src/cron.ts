4f65fb1753edd9cdab260ae5a084766a565c2959
Nathan Arthur <nathan@nathanarthur.com>
2024-02-21 14:41:42 -0500

    add cron log handling

--- src/cron.ts
+++ src/cron.ts
@@ -12,10 +12,14 @@ const HALF_HOUR = "0 */30 * * * *" as const;
 const TEN_MINUTES = "0 */10 * * * *" as const;
 const MINUTE = "0 * * * * *" as const;
 
-new Cron(HALF_HOUR, dynadone.update);
-new Cron(HALF_HOUR, dynanew.update);
-new Cron(MINUTE, bm.send);
-new Cron(MINUTE, tr.send);
-new Cron(TEN_MINUTES, bmGoal.update);
-new Cron(TEN_MINUTES, gross.update);
-new Cron(TEN_MINUTES, techtainment.update);
+const options = {
+  catch: (e: unknown) => console.error(e),
+};
+
+new Cron(HALF_HOUR, options, dynadone.update);
+new Cron(HALF_HOUR, options, dynanew.update);
+new Cron(MINUTE, options, bm.send);
+new Cron(MINUTE, options, tr.send);
+new Cron(TEN_MINUTES, options, bmGoal.update);
+new Cron(TEN_MINUTES, options, gross.update);
+new Cron(TEN_MINUTES, options, techtainment.update);
